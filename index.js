cd01b80b334fc75fdee82b44eac009ac1b726472
Anuj <anujrathor41@gmail.com>
2026-02-03 14:49:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log('first');
+// setTimeout(()=>{
+//     console.log("mid"),
+//     5000
+// })
+// console.log("end");
+
+import add from './add.js';
+
+
+const output=add(5,10);
+console.log(output);
