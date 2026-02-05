58c9e12716838c338e91137d5905cfd3e112af84
Anuj <anujrathor41@gmail.com>
2026-02-05 15:15:34 +0530

    os_module

--- add.js
+++ add.js
@@ -2,6 +2,7 @@ const add=(a,b)=>{
     return a+b;
 }
 const sub=(a,b)=>{
+    return a-b;
 
 }
 export default add;
--- index.js
+++ index.js
@@ -5,17 +5,22 @@
 // })
 // console.log("end");
 
-import add from './add.js';
+import  add ,{sub} from './add.js';
 import fs from 'fs';
 
 
-const output=add(5,10);
-console.log(output);
+// const output=add(5,10);
+// console.log(output);
 
-fs.readFile('notes.txt','utf8',(err,data)=>{
-    if(err){
-        console.log("error",err);
+// fs.readFile('notes.txt','utf8',(err,data)=>{
+//     if(err){
+//         // console.log("error",err);
+//         return;
 
-    }
-    console.log("data",data);
-})
+//     }
+//     console.log("data",data);
+// })
+
+import os from 'os';
+
+console.log(os.platform());
