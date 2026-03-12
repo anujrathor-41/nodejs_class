06a34e660b135ef58153e17af16b661f1a0a0fa9
Anuj <anujrathor41@gmail.com>
2026-03-12 15:26:07 +0530

    express2

--- modules/controller/userController.js
+++ modules/controller/userController.js
@@ -1,5 +1,11 @@
-const users=require('../modules/userModules');
+const users=require('../userModules');
 
 exports.getUsers=(req,res)=>{
     res.json(users);
+}
+
+exports.getUserById=(req,res)=>{
+    const user=users.find(u=>u.id===parseInt(req.params.id));
+    if(!user) return res.status(404).json({message:"User not found"});
+    res.json(user);
 }
