962f1792ed5b91661a63547f46a97e29c5a91234
Anuj <anujrathor41@gmail.com>
2026-03-26 15:07:08 +0530

    jwtIsUesd

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../modules/controller/authcontroller');
+
+const authMiddleware =(req,res,next)=>{
+    const token=req.headers.authorization;
+    if(!token){
+        return res.status(401).json({message: 'Access denied. No token provided.'});
+    }
+    try{
+        const decoded=jwt.verify(token, process.env.JWT_SECRET);
+        req.user=decoded;
+        next();
+    }catch(err){
+        res.status(400).json({message: 'Invalid token.'});
+    }
+}
